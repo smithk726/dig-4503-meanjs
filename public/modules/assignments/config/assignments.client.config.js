cdd371a9b2ccf8f2e6551430239a99fdb7dbb532
smithk726 <kaitlynsmith@Kaitlyns-iMac.local>
2015-04-08 21:00:29 -0400

    modified views and student views

--- public/modules/assignments/config/assignments.client.config.js
+++ public/modules/assignments/config/assignments.client.config.js
@@ -4,8 +4,8 @@
 angular.module('assignments').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Assignments', 'assignments', 'dropdown', '/assignments(/create)?');
-		Menus.addSubMenuItem('topbar', 'assignments', 'List Assignments', 'assignments');
-		Menus.addSubMenuItem('topbar', 'assignments', 'New Assignment', 'assignments/create');
+		Menus.addMenuItem('topbar', 'Assignments', 'assignments', '/assignments(/create)?');
+		/*Menus.addSubMenuItem('topbar', 'assignments', 'List Assignments', 'assignments');
+		Menus.addSubMenuItem('topbar', 'assignments', 'New Assignment', 'assignments/create');*/
 	}
 ]);
